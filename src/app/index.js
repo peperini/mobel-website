3f5c6dc0a8b1f412c885a43882135b0609e71152
José Félix <jarmando.felix@icloud.com>
2026-02-12 11:19:34 -0700

    Feat: Add header pug structure and some SCSS import and style fixes (#8)

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,23 @@
-const test = x => {
-  x = 'Hello Jose Felix'
-  return x
+import '@styles/index.scss'
+import FontFaceObserver from 'fontfaceobserver'
+
+class App {
+
 }
+
+const fontMontreal = new FontFaceObserver('pp_neue_montreal')
+const fontAntiPol = new FontFaceObserver('antipol_extended')
+const timeout = 2000
+
+Promise.all([fontMontreal.load(null, timeout), fontAntiPol.load(null, timeout)])
+  .then(() => {
+    window.APP = new App()
+  })
+  .catch(() => {
+    window.APP = new App()
+  })
+
+console.log(
+  '%c Design and Code by José Félix - https://josefelix.co/',
+  'background: #000; color: #fff;',
+)
