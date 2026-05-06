f4e6a2a9eb3649fa8e6b5bfdaefcfb2292c775c8
Jose Felix <jarmando.felix@icloud.com>
2026-05-06 15:54:36 -0700

    chore: pages and nav baseline

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,68 @@
 import '@styles/index.scss'
+
+import AutoBind from 'auto-bind'
 import FontFaceObserver from 'fontfaceobserver'
 import initGUI from './utils/grid-overlay'
-initGUI()
+
+import { Detection } from '@classes/Detection';
+
+import Navigation from '@components/Navigation'
+
+import About from '@pages/About'
+import Home from '@pages/Home'
+import Products from '@pages/Products'
 
 class App {
+  constructor() {
+    this.template = window.location.pathname
 
+    if (import.meta.env.DEV && window.location.search.indexOf('fps') > -1) {
+      this.createStats()
+    }
+
+    this.mouse = {
+      x: window.innerWidth / 2,
+      y: window.innerHeight / 2
+    }
+
+    AutoBind(this)
+
+    Detection.check({
+      onErrorWebGL: this.createUnsupportedScreen,
+      onSuccess: this.init
+    })
+  }
+
+  init () {
+    this.createNavigation()
+
+    this.createPages()
+    this.gui()
+  }
+
+  createNavigation() {
+    this.navigation = new Navigation({
+      template: this.template,
+    })
+  }
+
+  gui () {
+    initGUI()
+  }
+
+  createPages () {
+    this.about = new About()
+    this.products = new Products()
+    this.home = new Home()
+
+    this.pages = {
+      '/': this.home,
+      '/about': this.about,
+      '/products': this.products
+    }
+
+    this.page = this.pages[this.template]
+  }
 }
 
 const fontMontreal = new FontFaceObserver('pp_neue_montreal')
